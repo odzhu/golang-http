48ae6d962283803b8fc1639f522db81f5192d33d
Oleksii Dzhulai <nixlike@gmail.com>
2018-03-26 18:14:45 +0300

    Second linitial commit :)

--- hello.go
+++ hello.go
@@ -1,16 +1,15 @@
-
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "<html><head><title>Greetings to the world</title></head><body><h1>Hello from the Jenkins X golang http example</h1></body></html>\n")
+	fmt.Fprintf(w, "<html><head><title>Greetings to the world</title></head><body><h1>Hello from the Jenkins XXX golang http example</h1></body></html>\n")
 }
 
 func main() {
-    http.HandleFunc("/", handler)
-    http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
